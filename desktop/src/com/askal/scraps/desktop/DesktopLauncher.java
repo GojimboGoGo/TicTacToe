d9899f7619d2f620fae7ed353a3e5ae0753dec80
Joseph <joseph.miclatbourne@gmail.com>
2019-02-10 18:38:09 +0800

    Maintenance: Initial commit

---
+++ desktop/src/com/askal/scraps/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.askal.scraps.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.askal.scraps.TicTacToeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TicTacToeGame(), config);
+	}
+}
